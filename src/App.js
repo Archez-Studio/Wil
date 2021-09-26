866f077c23065970408c9b3fed3c479d703a7fcf
Joss Masaka <Masakajoseph@archez.studio>
2021-09-26 14:39:06 +0300

    Showcase area plus mods

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './components/HomePage/Nav'
+import Showcase from './components/HomePage/Showcase'
 
 
 const App = () => {
   return (
     <div>
       <Nav />
+      <Showcase />
     </div>
   )
 }
