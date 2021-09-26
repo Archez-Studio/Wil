86097222e9f796c689da031efbc0e7a1c6a79c04
Joss Masaka <Masakajoseph@archez.studio>
2021-09-26 15:06:05 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Nav from './components/HomePage/Nav'
 import Showcase from './components/HomePage/Showcase'
-
+import Footer from './components/HomePage/Footer'
 
 const App = () => {
   return (
     <div>
       <Nav />
       <Showcase />
+      <Footer />
     </div>
   )
 }
