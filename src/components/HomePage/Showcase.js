b7fa88a2ff0af7a1317e8e1711bde7ce6d5d7f9c
Joss Masaka <masakajoseph@archez.studio>
2021-11-17 20:39:19 +0300

    Showcase tabs mods

--- src/components/HomePage/Showcase.js
+++ src/components/HomePage/Showcase.js
@@ -3,6 +3,7 @@ import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
 import React from 'react'
 import { BrowserRouter as Router } from "react-router-dom"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 /*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'*/
 
@@ -17,42 +18,96 @@ const Showcase = () => {
                         <p>Find Your <br/>Prestigious Home</p>
                         <button ><a style={{ color: 'white', width: "100%", height: "100%" }} href="#Listing">Start Now</a></button>
                     
-                        <div className="tabs">
-                            <Link className="tab" to="/">
-                                Buy
-                            </Link>
-                            <Link className="tab" to="/">
-                                Sell
-                            </Link>
-                            <Link className="tab" to="/">
-                                Rent
-                            </Link>
-                            <Link className="tab" to="/">
-                                Lease
-                            </Link>
-                        </div>
-                        <div className="categories">
-                            <div className="category category-1">
-                                <label>City/Street</label><br/>
-                                <input type="text" name="name" placeholder="Enter City"/>
-                            </div><br/>
-                            <div className="category category-2">
-                                <label>Apartment Type</label><br/>
-                                <input type="text" name="name" placeholder="Deluxe Portion"/>
-                            </div>
-                            <div className="category category-3">
-                                <label>Price Range</label><br/>
-                                <input type="number" name="name" placeholder="Min Price - Max Price"/>
-                            </div>
-                            <div className="category category-4">
-                            <IconContext.Provider
-                                value={{ color: 'white', size: '25px', marginRight: '15px' }}
-                            >                   
-                                 
-                            </IconContext.Provider>
-                                <button className="search-button">Search</button>
-                            </div>
-                        </div>
+                        <Tabs className="tabs">
+                            <TabList>
+                                <Tab className="tab" to="/">
+                                    Buy
+                                </Tab>
+                                <Tab className="tab" to="/">
+                                    Sell
+                                </Tab>
+                                <Tab className="tab" to="/">
+                                    Rent
+                                </Tab>
+                                <Tab className="tab" to="/">
+                                    Lease
+                                </Tab>
+                            </TabList>
+
+                            <TabPanel>
+                                <div className="categories">
+                                    <div className="category category-1">
+                                        <label>City/Street</label><br/>
+                                        <input type="text" name="name" placeholder="Enter City"/>
+                                    </div><br/>
+                                    <div className="category category-2">
+                                        <label>Apartment Type</label><br/>
+                                        <input type="text" name="name" placeholder="Deluxe Portion"/>
+                                    </div>
+                                    <div className="category category-3">
+                                        <label>Price Range</label><br/>
+                                        <input type="number" name="name" placeholder="Min Price - Max Price"/>
+                                    </div>
+                                    <div className="category category-4">
+                                    <IconContext.Provider
+                                        value={{ color: 'white', size: '25px', marginRight: '15px' }}
+                                    >                   
+                                        
+                                    </IconContext.Provider>
+                                        <button className="search-button">Search</button>
+                                    </div>
+                                </div>
+                            </TabPanel>
+                            <TabPanel id="tabpanel-2">
+                                <div className="categories">
+                                    <div className="category category-1">
+                                        <label>Location</label><br/>
+                                        <input type="text" name="name" placeholder="Enter City"/>
+                                    </div><br/>
+                                    <div className="category category-2">
+                                        <label>Apartment Type</label><br/>
+                                        <input type="text" name="name" placeholder="Deluxe Portion"/>
+                                    </div>
+                                    <div className="category category-3">
+                                        <label>Price Range</label><br/>
+                                        <input type="number" name="name" placeholder="Min Price - Max Price"/>
+                                    </div>
+                                    <div className="category category-4">
+                                    <IconContext.Provider
+                                        value={{ color: 'white', size: '25px', marginRight: '15px' }}
+                                    >                   
+                                        
+                                    </IconContext.Provider>
+                                        <button className="search-button">Search</button>
+                                    </div>
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div className="categories">
+                                    <div className="category category-1">
+                                        <label>City/Street</label><br/>
+                                        <input type="text" name="name" placeholder="Enter City"/>
+                                    </div><br/>
+                                    <div className="category category-2">
+                                        <label>Apartment Type</label><br/>
+                                        <input type="text" name="name" placeholder="Deluxe Portion"/>
+                                    </div>
+                                    <div className="category category-3">
+                                        <label>Price Range</label><br/>
+                                        <input type="number" name="name" placeholder="Min Price - Max Price"/>
+                                    </div>
+                                    <div className="category category-4">
+                                        <IconContext.Provider
+                                            value={{ color: 'white', size: '25px', marginRight: '15px' }}
+                                        >                   
+                                            
+                                        </IconContext.Provider>
+                                        <button className="search-button">Search</button>
+                                    </div>
+                                </div>
+                            </TabPanel>
+                            
+                        </Tabs>
                     </div>
                 </div>
             </React.Fragment>
